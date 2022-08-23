66a0fb71c51225106216021bf4197fbaa267e5ed
Robert DeLaurentis <lists@bobdel.com>
2022-08-23 11:25:27 -0700

    Initial Commit

---
+++ GitHubExerciser/GitHubExerciserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciserApp.swift
+//  GitHubExerciser
+//
+//  Created by Robert DeLaurentis on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
